215bc51d9c634648ed6c2f0780646595a5dc8b21
Gustavo Oliveira <gustaoliveiradev@gmail.com>
2023-11-15 16:06:55 -0300

    função event por id funcionando

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,7 +1,7 @@
 import { MapPin } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react"; // Adicione o useState e useEffect para atualizar os dados
-import { GetAll } from "../services/api";
+import { GetAll, GetId } from "../services/api";
 
 interface SectionProps {
   title?: string;
@@ -9,6 +9,7 @@ interface SectionProps {
 
 export default function Section({ title }: SectionProps) {
   const [contentData, setContentData] = useState<any[]>([]);
+  const [eventId, setEventId] = useState<any[]>([]);
 
   useEffect(() => {
     GetAll()
@@ -20,33 +21,37 @@ export default function Section({ title }: SectionProps) {
       });
   }, []);
 
-  const ItemClick = (value: string) => {
-    console.log(value);
-  }
+  const getContentById = async (id: string) => {
+    await GetId(id)
+      .then((data) => {
+        setEventId(data);
+      })
+      .catch((error) => {
+        console.log("Erro ao obter dados por EVENT_ID:", error);
+      });
+  };
 
   return (
     <div className="">
-      {/* <Link to="/event"> */}
-        <div className="flex flex-row max-w-[1350px]">
-          {contentData.map((content, index) => (
-            <div
-              key={index}
-              className="w-[300px] h-[200px] flex flex-none flex-col items-center bg-primary border-2 border-primary mr-4"
-              onClick={() => ItemClick(content._id)}
-            >
-              <div className="flex flex-1 w-full bg-secondary"></div>
-              <div className="flex flex-1 flex-col w-full p-2 gap-y-1 bg-white text-black">
-                <div>{content.date}</div>
-                <h4>{content.title}</h4>
-                <div className="flex flex-row items-center">
-                  <MapPin size={18} color="#7E7E7E" />
-                  <div>{content.location}</div>
-                </div>
+      <div className="flex flex-row max-w-[1350px]">
+        {contentData.map((content, index) => (
+          <div
+            key={index}
+            className="w-[300px] h-[200px] flex flex-none flex-col items-center bg-primary border-2 border-primary mr-4"
+            onClick={() => getContentById(content._id)}
+          >
+            <div className="flex flex-1 w-full bg-secondary"></div>
+            <div className="flex flex-1 flex-col w-full p-2 gap-y-1 bg-white text-black">
+              <div>{content.date}</div>
+              <h4>{content.title}</h4>
+              <div className="flex flex-row items-center">
+                <MapPin size={18} color="#7E7E7E" />
+                <div>{content.location}</div>
               </div>
             </div>
-          ))}
-        </div>
-      {/* </Link> */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,23 @@
 import axios from "axios";
 
 export function GetAll() {
-  return axios.get("http://localhost:3333/event/get")
+  return axios
+    .get("http://localhost:3333/event/get")
     .then((response) => {
-      return response.data; 
+      return response.data;
     })
     .catch((error) => {
-      throw error; 
+      throw error;
+    });
+}
+
+export function GetId(id: String) {
+  return axios
+    .get(`http://localhost:3333/event/get${id}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error;
     });
 }
